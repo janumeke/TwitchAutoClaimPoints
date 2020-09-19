e7c7c9b7835c11aaa799218f90f824b9f81da933
janumeke <janumeke@gmail.com>
2020-09-19 18:25:09 +0800

    Add files via upload

---
+++ TwitchAutoClaimPoints_timer.user.js
@@ -0,0 +1,105 @@
+// ==UserScript==
+// @name         TwitchAutoClaimPoints
+// @namespace    https://github.com/janumeke/TwitchAutoClaimPoints
+// @version      0.3
+// @description  Auto click point bonus on Twitch.
+// @author       janumeke
+// @match        https://www.twitch.tv/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const log = true;
+    const debug = false; //If debug is set to true, the value of log will be ignored.
+
+    /*
+     It seems that if you claimed the bonus not too late than the time it had popped up,
+     Twitch would use the time it had popped up to calculate the time it should next pop up,
+     instead of the time you claimed the bonus.
+     This script will first try to find the time it just popped up, and then go back to the
+     normal interval.
+    */
+    const delayProbe = 15; //secs
+    const delayBonus = 900; //secs
+    const delayRsrv = (1 / 2) * delayBonus; //secs, assumed
+
+    var firstClaim; //This is set to true by 'Check' before calling 'Claim'
+    var autoclaim;
+    function Claim(){
+        const button = document.getElementsByClassName('claimable-bonus__icon')[0];
+        if(button){ //Bonus has popped up
+            button.click();
+            if(firstClaim){
+                firstClaim = false;
+                /*
+                 Suppose Twitch will only reserve delayRsrv seconds before forgetting the time it popped up.
+                 It means the time used for calculation (the time it popped up or you claimed) will be at most
+                 delayRsrv seconds ahead of the first successful claim. Therefore the time it should next pop up
+                 will be at least delayBonus - delayRsrv seconds later. If this starts probing before the next time
+                 it pops up, it will know the exact time it pops up (with at most delayProbe seconds positive offset)
+                 at the time of the second claim.
+                */
+                if(debug){
+                    let now = new Date();
+                    console.log(`TwitchAutoClaimPoints: (${now.toLocaleTimeString()}) Bonus points are claimed by the script. Next try is after ${delayBonus - delayRsrv - delayProbe} seconds.`);
+                }
+                else if(log){
+                    let now = new Date();
+                    console.log(`TwitchAutoClaimPoints: (${now.toLocaleTimeString()}) Bonus points are claimed by the script.`);
+                }
+                autoclaim = setTimeout(Claim, (delayBonus - delayRsrv - delayProbe) * 1000);
+            }
+            else{
+                if(debug){
+                    let now = new Date();
+                    console.log(`TwitchAutoClaimPoints: (${now.toLocaleTimeString()}) Bonus points are claimed by the script. Next try is after ${delayBonus - delayProbe} seconds.`);
+                }
+                else if(log){
+                    let now = new Date();
+                    console.log(`TwitchAutoClaimPoints: (${now.toLocaleTimeString()}) Bonus points are claimed by the script.`);
+                }
+                autoclaim = setTimeout(Claim, (delayBonus - delayProbe) * 1000);
+            }
+        }
+        else{ //Bonus is not available, start probing
+            if(debug){
+                let now = new Date();
+                console.log(`TwitchAutoClaimPoints: Bonus points cannot be found by the script. Next try is after ${delayProbe} seconds.`);
+            }
+            autoclaim = setTimeout(Claim, delayProbe * 1000);
+        }
+        //Invariant: Exact one 'setTimeout' that will call this function is waiting.
+    }
+
+    function Check(){ //This function will be called when the route is changed
+        /*
+         Since Javascript is single-threaded, this function is called only if 'Claim' hasn't executed or has finished execution.
+         Therfore, autoclaim points to the only timed execution that was invoked by the last 'Claim', if 'Claim' had been first invoked by 'Check'.
+        */
+        clearTimeout(autoclaim);
+
+        const regexReserved = new RegExp('^/(directory|videos|downloads|broadcast|p|creatorcamp|store|partner|jobs|bits|subs|prime|legal|turbo|products|redeem|search|settings|friends|subscriptions|inventory|drops|wallet)(/.*)?$');
+        const regexChannel = new RegExp('^/[A-Za-z0-9_]+(/(about|schedule|videos|clips|collections)?)?$');
+        if(location.hostname == 'www.twitch.tv' && //This script may still be triggerred on subdomains other than "www"
+           !regexReserved.test(location.pathname) &&
+           regexChannel.test(location.pathname)){
+            firstClaim = true;
+            Claim();
+        }
+        else{
+            if(debug){
+                console.log('TwitchAutoClaimPoints: The script determines this page is not a channel. The automatic point claim will not begin.');
+            }
+        }
+    }
+    Check(); //Always check when the whole page first loads.
+
+    const pushState = history.pushState;
+    history.pushState = function(){ //When single page applications change routes
+        pushState.apply(history, arguments);
+        Check();
+    };
+    window.addEventListener('popstate', Check); //When navigating with the back/forward buttons
+})();
---
+++ TwitchAutoClaimPoints_watcher.user.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name         TwitchAutoClaimPoints
+// @namespace    https://github.com/janumeke/TwitchAutoClaimPoints
+// @version      0.1
+// @description  Auto click point bonus on Twitch.
+// @author       janumeke
+// @match        https://www.twitch.tv/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const log = true;
+    const debug = false; //If debug is set to true, the value of log will be ignored.
+
+    function Claim(){
+        const button = document.getElementsByClassName('claimable-bonus__icon')[0];
+        if(button){
+            button.click();
+            if(log || debug){
+                let now = new Date();
+                console.log(`TwitchAutoClaimPoints: (${now.toLocaleTimeString()}) Bonus points are claimed by the script.`);
+            }
+        }
+    }
+
+    const observer = new MutationObserver(function(){
+        if(debug){
+            console.log("TwitchAutoClaimPoints: Target change is detected.");
+        }
+        Claim();
+    });
+
+    var tryWatch;
+    function Watch(){ //This function will be called repeatedly until itself decides it's successful
+        observer.disconnect();
+
+        const target = document.querySelector('div.community-points-summary').lastChild;
+        if(target){
+            clearInterval(tryWatch);
+            Claim(); //Try claim in case the button has existed.
+
+            observer.observe(target, {childList: true});
+            if(debug){
+                console.log('TwitchAutoClaimPoints: Target is being watched.');
+            }
+        }
+        //Invariant: observer observes at most one target
+    }
+
+    function Check(){ //This function will be called when the route is changed
+        clearInterval(tryWatch);
+
+        const regexReserved = new RegExp('^/(directory|videos|downloads|broadcast|p|creatorcamp|store|partner|jobs|bits|subs|prime|legal|turbo|products|redeem|search|settings|friends|subscriptions|inventory|drops|wallet)(/.*)?$');
+        const regexChannel = new RegExp('^/[A-Za-z0-9_]+(/(about|schedule|videos|clips|collections)?)?$');
+        if(location.hostname == 'www.twitch.tv' && //This script may still be triggerred on subdomains other than "www"
+           !regexReserved.test(location.pathname) &&
+           regexChannel.test(location.pathname)){
+            tryWatch = setInterval(Watch, 5000);
+        }
+        else{
+            if(debug){
+                console.log('TwitchAutoClaimPoints: The script determines this page is not a channel. The automatic point claim will not begin.');
+            }
+        }
+        //Invariant: At most one 'setInterval' of 'Watch' is running
+    }
+    Check(); //Always check when the whole page first loads
+
+    const pushState = history.pushState;
+    history.pushState = function(){ //When single page applications change routes
+        pushState.apply(history, arguments);
+        Check();
+    };
+    window.addEventListener('popstate', Check); //When navigating with the back/forward buttons
+})();
